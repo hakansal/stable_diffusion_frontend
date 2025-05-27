cff76f5dbb8ea63d833cb852fe17eb5e075f9b4b
hakansal <hakansal3149@yandex.com>
2025-05-27 22:57:48 +0300

    navbar ve hakk  update

--- src/Components/hakk/hakk.jsx
+++ src/Components/hakk/hakk.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 import "./hakk.scss"
 
-const Hak=()=>{
+const Hak = () => {
 
 
     return <div className="hakk_main">
- <div className="hakk_container">
+        <div className="hakk_container">
+           
+           <div className="info_hakk">
+             
 
- </div>
+           </div>
+         
+        </div>
+          <div className="hakk_container">
+           
+           <div className="info_hakk">
+             
+             
+           </div>
+         
+        </div>
+          <div className="hakk_container">
+           
+           <div className="info_hakk">
+             
+             
+           </div>
+         
+        </div>
     </div>
 }
 export default Hak;
